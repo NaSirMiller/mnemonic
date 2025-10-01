14f5b27c3886429c834142124099affa80936654
NaSirMiller <nmiller1329@gmail.com>
2025-10-01 01:49:11 -0400

    Moved src/home.html into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
-
+import "./style.css"; // Make sure your CSS file is in the same folder or adjust the path
 
 function App() {
+  return (
+    <div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/Courses">Courses</a>
+        <a href="/Tasks">Tasks</a>
+      </nav>
 
-    return (
-        <>
-            Hello World
-        </>
-    )
+      <main id="Home">
+        <button type="button">Connect Calendar</button>
+      </main>
+    </div>
+  );
 }
 
 export default App;
