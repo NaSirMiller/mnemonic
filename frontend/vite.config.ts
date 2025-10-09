9632283fdbb13db5c4f4e714023f03d18ac2ed3e
vincent-lau <vincentlau31545@gmail.com>
2025-10-09 17:33:34 -0400

    attempt to fix css

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    {
+      name: 'ignore-css',
+      enforce: 'pre',
+      load(id) {
+        if (id.endsWith('.css')) return 'export default {};';
+      },
+    },
+  ],
+});
