412f975f1fcfd24da7ce18635b7803ff6364132e
Na'Sir Miller <nmiller1329@gmail.com>
2025-10-15 11:31:36 -0400

    Mnem 54 create firestore schemas for assignment course assignment progress task tasklist (#9)
    * Added firebase.rules for schema validation and tested
    
    * Removed project id from test and added command to run tests
    
    * added jest dependency
    
    ---------
    
    Co-authored-by: iking919 <153115930+iking919@users.noreply.github.com>

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,14 +1,9 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
   testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
 };
