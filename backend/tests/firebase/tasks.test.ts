20030be421e48b6f1a4b3b8192a58c1ac6e35a16
NaSirMiller <nmiller1329@gmail.com>
2025-10-22 14:02:28 -0400

    Created task test file

---
+++ backend/tests/firebase/tasks.test.ts
@@ -0,0 +1,9 @@
+import {
+  initializeTestEnvironment,
+  assertSucceeds,
+  assertFails,
+} from "@firebase/rules-unit-testing";
+const fs = require("fs");
+const path = require("path");
+import { config } from "dotenv";
+config();
