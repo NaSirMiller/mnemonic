7ca0032843adc16fd489f3bcda3050ce8694ba9e
vincent31545 <122557562+vincent31545@users.noreply.github.com>
2025-10-30 13:03:20 -0400

    Home page (#13)
    * completed home page without task data and without error checking on length of task name
    
    calendar also does not reflect tasks
    
    * task page done
    
    * completed ui 80%
    
    ---------
    
    Co-authored-by: iking919 <153115930+iking919@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,35 @@
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
+// import { HomePage } from "./pages/HomePage";
 import { LoginPage } from "./pages/LoginPage";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import TaskPage from "./TaskPage/TaskPage.tsx";
+import HomePage from "./HomePage/HomePage.tsx";
+
 const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <HomePage />
-      </ProtectedRoute>
-    ),
-  },
-  {
-    path: "/auth",
-    element: <LoginPage />,
-  },
+    {
+        path: "/",
+        element: (
+            // <ProtectedRoute>
+                <HomePage />
+            // </ProtectedRoute>
+        ),
+    },
+    {
+        path: "/auth",
+        element: <LoginPage />,
+    },
+    {
+        path: "/tasks",
+        element: <TaskPage />,
+    },
 ]);
 
 function App() {
-  return <RouterProvider router={appRouter}></RouterProvider>;
+    return <RouterProvider router={appRouter} />;
+    // return <FrontEndHomePage />;
+    // return <TaskPage />;
 }
 
 export default App;
--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -1,38 +1,58 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import NavBarButton from "./NavBarButton.tsx";
 import "./NavBar.css";
 
 function NavBar() {
-    const [menuOpen, setMenuOpen] = useState(false); // controls mobile nav bar
-    const [clickedButton, setClickedButton] = useState<string>("Home"); // controls focused tab
-    const buttonTexts = ["Home", "Courses", "Tasks", "Sign Up", "Log In"]; // array of buttons
+    const [ menuOpen, setMenuOpen ] = useState( false ); // controls mobile nav bar
+    const [ clickedButton, setClickedButton ] = useState<string>( "Home" ); // controls focused tab
+    const buttonTexts = [ "Home", "Tasks" ]; // array of buttons
+
+    const navigate = useNavigate();
 
     const handleClick = ( text: string ) => {
-        setClickedButton(text);
+        if ( text === "Home" )
+            navigate( "/" );
+        if ( text === "Tasks" )
+            navigate( "/tasks" );
+        setClickedButton( text );
     };
 
     return (
         <div className="navbar-cont">
-            <div className="navbar-logo">Mnemonic Logo</div>
+            <img 
+                src={ "/images/logo-removebg.png" }  
+                alt="mnemonic logo" 
+                className="navbar-logo"
+                onClick={ () => handleClick( "Home" ) }
+            />
 
             {/* Hamburger button for small screens */}
             <div 
                 className="navbar-hamburger"
-                onClick={ () => setMenuOpen(!menuOpen) }
+                onClick={ () => setMenuOpen( !menuOpen ) }
             >
                 ☰
             </div>
 
             {/* Buttons container */}
-            <div className={`navbar-buttons-cont ${menuOpen ? "open" : ""}`}>
+            <div className={ `navbar-buttons-cont ${ menuOpen ? "open" : "" }` }>
                 {buttonTexts.map( ( text, i ) => (
                     <NavBarButton
                         key={ text + i }
                         text={ text }
                         focused={ clickedButton === text }
-                        onClick={ () => handleClick(text) }
+                        onClick={ () => handleClick( text ) }
                     />
                 ))}
+
+                
+            </div>
+            {/* <div className="navbar-sign-in">
+                Sign In
+            </div> */}
+            <div className="navbar-sign-in">
+                Log Out
             </div>
         </div>
     );
